bfa9ee8cf2e00f0b4c1ce60cf67d498c21e97200
MeibisuX673 <g1eme.please@gmail.com>
2021-09-04 19:41:17 +0300

    update message swiftMailerInterface

--- photoApi/src/Services/SwiftMailerService.php
+++ photoApi/src/Services/SwiftMailerService.php
@@ -15,21 +15,12 @@ class SwiftMailerService implements SwiftMailerServiceInterface
 
     public function sendMessage(string $email)
     {
-        $message = (new \Swift_Message('Hello Email'))
-            ->setFrom('test.test@test.com','GAY')
-            ->setTo($email)
-            ->setBody('<p>See Twig integration for better HTML integration!</p>');
-
-        $this->mailer->send($message);
-    }
-
-    public function sendMessageTest(string $email, $message)
-    {
-        $message = (new \Swift_Message("You have registered'))
+        $message = (new \Swift_Message('Hello!!'))
             ->setFrom('test.test@test.com','Test')
             ->setTo($email)
-            ->setBody($message);
+            ->setBody('You have registered');
 
         $this->mailer->send($message);
     }
+
 }
